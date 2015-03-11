b5ffe57923a699d3875d805027722cf77601af54
Cynthia Wilhelm & Tom Mertz <student@epicodus.com>
2015-03-11 16:35:24 -0700

    Add partial match functionailty to find-replace method

--- src/FindReplace.php
+++ src/FindReplace.php
@@ -4,8 +4,8 @@
 
         function replaceTarget($main, $target, $replacement)
         {
-            $pattern = "*" . $target . "*";
-            return preg_replace($pattern, $replacement, $main);
+            // $pattern = "*" . $target . "*";
+            // return preg_replace($pattern, $replacement, $main);
             // $temp = explode(" ", $main);
             // $results = array();
             // foreach($temp as $word) {
@@ -16,6 +16,18 @@
             //     }
             // }
             // return implode(" ", $results);
+
+            $target_size = strlen($target);
+            $final = $main;
+
+            for ($i = 0; $i < strlen($main); $i++) {
+                $main_temp = substr($main, $i, $target_size);
+
+                if ($main_temp == $target) {
+                    $final = substr($final, 0, $i) . $replacement . substr($final, ($i + $target_size), (strlen($main) - ($i + $target_size)));
+                }
+            }
+            return $final;
         }
     }
 
